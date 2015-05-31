3090f5852902891061f27d68d8c66b006b5916f3
= <=>
2015-05-31 11:20:29 -0600

    License + setup crap

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='bcslib',
+    version='0.0.1',
+    license='LICENSE',
+    description='Doing stuff with the BCS-462',
+    install_requires=['requests>=2.7.0']
+)
